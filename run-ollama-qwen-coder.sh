175e82bfb0a394ebb9aab7508615aadaa043a84c
Chanwit <chanwit.kepha@outlook.com>
2025-02-04 12:35:47 +0700

    Add shell script to run Qwen2.5 and Qwen2.5-Coder 0.5B.

--- run-ollama-qwen-coder.sh
+++ run-ollama-qwen-coder.sh
@@ -5,4 +5,4 @@
 # But for smooth performance, I think we should use VM specs 4 vCPU and 8 GB
 
 docker compose exec ollama-docker ollama list
-docker compose exec ollama-docker ollama run qwen2.5:0.5B
+docker compose exec ollama-docker ollama run qwen2.5-coder:0.5B
